f9f7ec51d441b1f76d8dab7c272c2514a2030690
Joe Kirchhof <joe@obscurae.com>
2018-06-08 16:01:12 -0400

    new storage interface and basic implementation with WP "meta data"

--- src/Storage/StorageInterface.php
+++ src/Storage/StorageInterface.php
@@ -7,4 +7,57 @@ namespace CustomFields\Storage;
  */
 interface StorageInterface {
 
+  /**
+   * For a specific post ID, persist a key-value pair to storage.
+   *
+   * @param int $postId
+   *   ID of post related to key-value pair.
+   * @param string $key
+   *   Key of stored value.
+   * @param mixed $value
+   *   Value to store. Scalar and boolean values may be converted to strings.
+   *   Other values may be serialized. Values may also be escaped.
+   *
+   * @return bool
+   *   TRUE on success; FALSE on failure.
+   */
+  public function persist(int $postId, string $key, $value);
+
+  /**
+   * For a specific post ID and key, get its value from storage.
+   *
+   * @param int $postId
+   *   ID of post related to key-value pair.
+   * @param string $key
+   *   Key of stored value.
+   *
+   * @return mixed
+   *   Stored value, possibly unserialized and/or unescaped.
+   */
+  public function retrieve(int $postId, string $key);
+
+  /**
+   * For a specific post ID, get all key-value pairs from storage.
+   *
+   * @param int $postId
+   *   ID of post related to key-value pair.
+   *
+   * @return array
+   *   Stored value, possibly unserialized and/or unescaped.
+   */
+  public function retrieveAll(int $postId);
+
+  /**
+   * For a specific post ID and key, delete its value from storage.
+   *
+   * @param int $postId
+   *   ID of post related to key-value pair.
+   * @param string $key
+   *   Key of stored value.
+   *
+   * @return bool
+   *   TRUE on success; FALSE on failure.
+   */
+  public function delete(int $postId, string $key);
+
 }
--- src/Storage/WPMetaData.php
+++ src/Storage/WPMetaData.php
@@ -7,4 +7,42 @@ namespace CustomFields\Storage;
  */
 class WPMetaData implements StorageInterface {
 
+  /**
+   * {@inheritdoc}
+   */
+  public function persist(int $postId, string $key, $value) {
+    // Return value of update_post_meta is ignored. FALSE is returned on both
+    // failure and when previous value is same as new value, with no distinction
+    // for the apparent error.
+    update_post_meta($postId, $key, $value);
+    $storedValue = $this->retrieve($postId, $key);
+    if (is_scalar($storedValue)) {
+      return (string) $value == (string) $storedValue;
+    }
+    else {
+      return $value == $storedValue;
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function retrieve(int $postId, string $key) {
+    return get_post_meta($postId, $key, TRUE);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function retrieveAll(int $postId) {
+    return get_post_meta($postId);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function delete(int $postId, string $key) {
+    return delete_post_meta($postId, $key);
+  }
+
 }
