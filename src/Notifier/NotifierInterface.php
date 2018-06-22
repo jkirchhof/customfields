11c9fc34ff30f6ff497f73c009b7005364eaf521
Joe Kirchhof <joe@obscurae.com>
2018-06-22 15:44:23 -0400

    Notifier methods to handle user notices

--- src/Notifier/NotifierInterface.php
+++ src/Notifier/NotifierInterface.php
@@ -8,11 +8,60 @@ namespace CustomFields\Notifier;
 interface NotifierInterface {
 
   /**
+   * Set default transient key.
+   *
+   * @param string $key
+   *   Key of transient.
+   *
+   * @return static
+   */
+  public function setTranientKey(string $key);
+
+  /**
    * Show notice on admin pages.
    *
    * @param string $message
    *   Text to display in notice.
+   *
+   * @return static
    */
   public function queueAdminNotice(string $message);
+
+  /**
+   * Add message to display to user.
+   *
+   * @param string $message
+   *   User message to queue.
+   * @param string $transientKey
+   *   Transient key for saving message. Implementation should default to
+   *   $this->transientKey.
+   *
+   * @return static
+   */
+  public function queueUserWarning(string $message, string $transientKey);
+
+  /**
+   * Add field to list of those to flag.
+   *
+   * @param string $field
+   *   Machine name of field.
+   * @param string $transientKey
+   *   Transient key for saving message. Implementation should default to
+   *   $this->transientKey.
+   *
+   * @return static
+   */
+  public function queueFieldWarning(string $field, string $transientKey);
+
+  /**
+   * Retrieve warnings from storage; remove value from storage.
+   *
+   * @param string $transientKey
+   *   Key of transient.
+   *
+   * @return mixed
+   *   Warnings.
+   */
+  public function retrieveWarnings(string $transientKey);
 
 }
--- src/Notifier/WPNotifier.php
+++ src/Notifier/WPNotifier.php
@@ -8,6 +8,51 @@ namespace CustomFields\Notifier;
 class WPNotifier implements NotifierInterface {
 
   /**
+   * Array keyed by transient key. Children are arrays of user message strings.
+   *
+   * @var array
+   */
+  protected $userWarnings = [];
+
+  /**
+   * Array keyed by transient key. Children are arrays of fields to flag.
+   *
+   * @var array
+   */
+  protected $fieldWarnings = [];
+
+  /**
+   * Default transient key for warnings.
+   *
+   * @var string
+   */
+  protected $transientKey = '';
+
+  /**
+   * Store transient values on destruction.
+   */
+  public function __destruct() {
+    if (count($this->userWarnings) || count($this->fieldWarnings)) {
+      $transientKeys = array_unique(array_keys($this->userWarnings) + array_keys($this->fieldWarnings));
+      foreach ($transientKeys as $transientKey) {
+        $transientValue = [
+          'messages' => $this->userWarnings[$transientKey] ?: [],
+          'elements' => $this->fieldWarnings[$transientKey] ?: [],
+        ];
+        set_transient($transientKey, $transientValue, 30);
+      }
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setTranientKey(string $key) {
+    $this->transientKey = $key;
+    return $this;
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function queueAdminNotice(string $message) {
@@ -15,6 +60,41 @@ class WPNotifier implements NotifierInterface {
       $message = __($message);
       echo '<div class="notice notice-error"><p>' . $message . '</p></div>';
     });
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function queueUserWarning(string $message, string $transientKey = '') {
+    if ($transientKey == '') {
+      $transientKey = $this->transientKey;
+    }
+    $this->userWarnings[$transientKey][] = $message;
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function queueFieldWarning(string $field, string $transientKey = '') {
+    if ($transientKey == '') {
+      $transientKey = $this->transientKey;
+    }
+    $this->fieldWarnings[$transientKey][] = $field;
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function retrieveWarnings(string $transientKey = '') {
+    if ($transientKey == '') {
+      $transientKey = $this->transientKey;
+    }
+    $warnings = get_transient($transientKey);
+    // delete_transient($transientKey);
+    return $warnings;
   }
 
 }
